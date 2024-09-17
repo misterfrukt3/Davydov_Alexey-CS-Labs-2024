9d45b2dd5bb91915e080f0684b932cc1c5239956
misterfrukt3 <misterfrukt@DESKTOP-GC39LOM.>
2024-09-17 18:21:50 +0300

    modifief Davydov_A_lab_1.cpp

--- Davydov_A_lab_1.cpp
+++ Davydov_A_lab_1.cpp
@@ -1,98 +1,93 @@
 #include <iostream>
-namespace {
-const int kTestCaseOne = 1;
-const int kTestCaseFive = 5;
-const int kTestCaseTen = 10;
-const int kTestCaseEleven = 11;
-const int kTestCaseTwelve = 12;
-const int kTestCaseTwenty = 20;
 
+namespace {
 const int kMorningStart = 5;
 const int kHalfADay = 12;
 const int kEveningStart = 18;
 const int kEveningEnd = 23;
 
-const int kTenForRest = 10;
-const int kTwelveForRest = 12;
-
+const int kMinHour = 0;
 const int kMaxHour = 23;
+
+const int kMinMinute = 0;
 const int kMaxMinute = 59;
+
+const int kHourNominativeSingular = 1;
+const int kHourGenitiveSingularLeftBoundary = 1;
+const int kHourGenitiveSingularRightBoundary = 5;
+
+const int kMinuteNominativeSingular = 1;
+const int kMinuteGenetivePluralStart = 11;
+const int kMinuteNominativePluralLeftBoundary = 1;
+const int kMinuteNominativePluralRightBoundary = 5;
+const int kMinuteGenetivePluralStart = 11;
+const int kMinuteGenetivePluralEnd = 20;
+
+const int kBaseDecimal = 10;
+const int kBaseHoursEnglishFormat = 12;
 }  // namespace
 
 int main() {
-    int hoursEuropeanFormate = 0;
-    int hoursEnglishFormate = 0;
-    int enterMinute = 0;
-    std::cout << "Введите время: \n";
-    std::cin >> hoursEuropeanFormate >> enterMinute;
+    int hoursEuropeanFormat = 0;
+    int minutes = 0;
+    std::cout << "Введите часы (0<=x<=23) и минуты (0<=y<=59) через пробел: \n";
+    std::cin >> hoursEuropeanFormat >> minutes;
 
-    if (hoursEuropeanFormate < 0 || hoursEuropeanFormate > kMaxHour || enterMinute < 0 || enterMinute > kMaxMinute) {
+    if (hoursEuropeanFormat < kMinHour || hoursEuropeanFormat > kMaxHour || minutes < kMinMinute || minutes > kMaxMinute) {
         std::cout << "Введены недопустимые данные\n";
         return -1;
     }
-    if (hoursEuropeanFormate == kHalfADay && enterMinute == 0) {
+
+    if (hoursEuropeanFormat == kHalfADay && minutes == kMinMinute) {
         std::cout << "Полдень\n";
         return 0;
     }
-    if (hoursEuropeanFormate == 0 && enterMinute == 0) {
+    if (hoursEuropeanFormat == kMinHour && minutes == kMinMinute) {
         std::cout << "Полночь\n";
         return 0;
     }
 
-    hoursEnglishFormate = hoursEuropeanFormate % kTwelveForRest;
-    if (hoursEuropeanFormate < kTestCaseFive && hoursEuropeanFormate > 0) {
-        std::cout << hoursEnglishFormate << " ";
+    int hoursEnglishFormat = hoursEuropeanFormat % kBaseHoursEnglishFormat;
+    if (hoursEuropeanFormat == kHalfADay) {
+        std::cout << kHalfADay << " ";
     } else {
-        if (hoursEuropeanFormate == kHalfADay) {
-            hoursEnglishFormate = kHalfADay;
-            std::cout << hoursEnglishFormate << " ";
-        } else {
-            std::cout << hoursEnglishFormate << " ";
-        }
+        std::cout << hoursEnglishFormat << " ";
     }
 
-    if (hoursEnglishFormate == kTestCaseOne) {
+    if (hoursEnglishFormat == kHourNominativeSingular) {
         std::cout << "час ";
-    } else if (kTestCaseOne < hoursEnglishFormate && hoursEnglishFormate < kTestCaseFive) {
+    } else if (hoursEnglishFormat > kHourGenitiveSingularLeftBoundary && hoursEnglishFormat < kHourGenitiveSingularRightBoundary) {
         std::cout << "часа ";
     } else {
         std::cout << "часов ";
     }
 
-    if (enterMinute != 0) {
-        std::cout << enterMinute;
-        if (enterMinute % kTestCaseTen == kTestCaseOne && enterMinute != kTestCaseEleven) {
+    if (minutes != kMinMinute) {
+        std::cout << minutes;
+        if (minutes % kBaseDecimal == kMinuteNominativeSingular && minutes != kMinuteGenetivePluralStart) {
             std::cout << " минута ";
-        } else if (kTestCaseOne < enterMinute % kTenForRest && enterMinute % kTenForRest < kTestCaseFive &&
-                   (kTestCaseTen > enterMinute || enterMinute > kTestCaseTwenty)) {
+        } else if (minutes % kBaseDecimal > kMinuteNominativePluralLeftBoundary && minutes % kBaseDecimal < kMinuteNominativePluralRightBoundary &&
+                   !(minutes > kMinuteGenetivePluralStart && minutes < kMinuteGenetivePluralEnd)) {
             std::cout << " минуты ";
         } else {
             std::cout << " минут ";
         }
+    }
 
-        if (kMorningStart <= hoursEuropeanFormate && hoursEuropeanFormate < kHalfADay) {
-            std::cout << "утра \n";
-            return 0;
-        } else if (kHalfADay <= hoursEuropeanFormate && hoursEuropeanFormate < kEveningStart) {
-            std::cout << "дня \n";
-            return 0;
-        } else if (kEveningStart <= hoursEuropeanFormate && hoursEuropeanFormate <= kMaxHour) {
-            std::cout << "вечера \n";
-            return 0;
-        }
-        std::cout << "ночи \n";
-        return 0;
+    if (hoursEuropeanFormat >= kMorningStart && hoursEuropeanFormat < kHalfADay) {
+        std::cout << "утра ";
+    } else if (hoursEuropeanFormat >= kHalfADay && hoursEuropeanFormat < kEveningStart) {
+        std::cout << "дня ";
+    } else if (hoursEuropeanFormat >= kEveningStart && hoursEuropeanFormat <= kMaxHour) {
+        std::cout << "вечера ";
+    } else {
+        std::cout << "ночи ";
     }
-    if (kMorningStart <= hoursEuropeanFormate && hoursEuropeanFormate < kHalfADay) {
-        std::cout << "утра ровно \n";
-        return 0;
-    } else if (kHalfADay <= hoursEuropeanFormate && hoursEuropeanFormate < kEveningStart) {
-        std::cout << "дня ровно \n";
-        return 0;
-    } else if (kEveningStart <= hoursEuropeanFormate && hoursEuropeanFormate <= kMaxHour) {
-        std::cout << "вечера ровно \n";
-        return 0;
+
+    if (minutes == kMinMinute) {
+        std::cout << "ровно";
     }
-    std::cout << "ночи ровно \n";
+
+    std::cout << '\n';
     return 0;
 }
