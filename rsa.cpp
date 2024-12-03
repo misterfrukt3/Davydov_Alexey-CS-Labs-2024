bb1d4342c30f102da438f7b1bdb68cbb53ca4521
misterfrukt3 <misterfrukt@DESKTOP-GC39LOM.>
2024-12-03 23:38:28 +0300

    modified rsa.cpp rsa.h main.cpp

--- rsa.cpp
+++ rsa.cpp
@@ -7,25 +7,29 @@ namespace RSA {
 [[nodiscard]] PrimeDividers CalculatePrimeFact(int num) {
     for (int i = 2; (i * i < num); ++i) {
         if (num % i == 0) {
+            std::cout<<i<<" "<<num/i<<std::endl;
             return {i, num / i};
         }
     }
-
+    PrimeDividers result = {0,0};
     std::cout << "Не удалось разложить на простые множители\n";
-    return {};
+    return result;
 }
 
 [[nodiscard]] Key CalculatePrivKey(Key publicKey) {
     PrimeDividers Dividers = CalculatePrimeFact(publicKey.N);
-
+    if (Dividers.q == 0 && Dividers.p == 0){
+        Key result = {0, 0};
+        return result;
+    }
     int EulerFunction = (Dividers.q - 1) * (Dividers.p - 1);
     int i = 2;
 
     while (publicKey.e * i % EulerFunction != 1) {
         ++i;
     }
-
-    return {i, publicKey.N};
+    Key result = {i, publicKey.N};
+    return result;
 }
 
 [[nodiscard]] int DecryptMessage(Key privKey, int message) {
@@ -51,8 +55,11 @@ void RunApplication() {
     std::cin >> mess;
 
     RSA::Key privKey = RSA::CalculatePrivKey(publicKey);
+    if (privKey.e == 0 && privKey.N == 0){
 
+    } else {
     std::cout << "Расшифрованное сообщение:" << std::endl;
     std::cout << RSA::DecryptMessage(privKey, mess) << std::endl;
+    }
 }
 }  // namespace RSA
