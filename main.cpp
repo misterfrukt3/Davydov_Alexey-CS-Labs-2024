c6355513f93be1acba7ba3eca8c1c2b3d9070fb3
misterfrukt3 <misterfrukt@DESKTOP-GC39LOM.>
2024-12-03 14:45:36 +0300

    modified rsa.cpp rsa.h main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,7 @@
 #include "rsa.h"
 
 int main(int, char**) {
-    RunApplication();
-    RSA::Key publicKey;
-
-    std::cout << "Введите открытый ключ через пробел(пару чисел (e, N))" << std::endl;
-    std::cin >> publicKey.e >> publicKey.N;
-
-    int mess = 0;
-    std::cout << "Введите зашифрованное сообщение:" << std::endl;
-    std::cin >> mess;
-
-    RSA::Key privKey = RSA::CalculatePrivKey(publicKey);
-
-    std::cout << "Расшифрованное сообщение:" << std::endl;
-    std::cout << RSA::DecryptMessage(privKey, mess) << std::endl;
+    RSA::RunApplication();
 
     return 0;
 }
--- rsa.cpp
+++ rsa.cpp
@@ -41,6 +41,18 @@ namespace RSA {
 }
 
 void RunApplication() {
-    
+    RSA::Key publicKey;
+
+    std::cout << "Введите открытый ключ через пробел(пару чисел (e, N))" << std::endl;
+    std::cin >> publicKey.e >> publicKey.N;
+
+    int mess = 0;
+    std::cout << "Введите зашифрованное сообщение:" << std::endl;
+    std::cin >> mess;
+
+    RSA::Key privKey = RSA::CalculatePrivKey(publicKey);
+
+    std::cout << "Расшифрованное сообщение:" << std::endl;
+    std::cout << RSA::DecryptMessage(privKey, mess) << std::endl;
 }
 }  // namespace RSA
